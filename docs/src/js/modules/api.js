f2ecfdd71732c1d4cfb9bada6de469c949bfb58e
MarjoleinAardewijn <marjolein.aardewijn@gmail.com>
2020-03-16 14:32:30 +0100

    refactor code to using modules and trying to use .env file (not working for API_KEY)

--- docs/src/js/modules/api.js
+++ docs/src/js/modules/api.js
@@ -1,55 +1,15 @@
-import {data as dataModule} from "./data.js";
+const fetch = require('node-fetch');
 
-const endpoint = 'https://www.rijksmuseum.nl/api/nl/collection',
-    apiKey = 'n0Iu86hl';
+async function overview(url) {
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    return jsonData.artObjects;
+}
 
-export const api = {
-    /**
-     * Async function to fetch all the paintings from a specific painter and the details
-     * of every painting.
-     *
-     * @param artist: the artist where you want to see some paintings from.
-     */
-    getData: async function(artist) {
-        const urlArtist = `${endpoint}?key=${apiKey}&involvedMaker=${artist}`;
+async function details(url) {
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    return jsonData.artObject;
+}
 
-        try {
-            /**
-             * Fetch paintings and the details from an artist.
-             *
-             * @type {Response} -> Paintings from artist + details.
-             */
-            const response = await fetch(urlArtist);
-            const jsonData = await response.json();
-            const objects = jsonData.artObjects;
-
-            // Check if paintings are from the artist (filter) and loop through the paintings
-            // to get the details of every painting (map).
-            const details = objects.filter(async function (object) {
-                return object.principalOrFirstMaker === artist;
-            }).map(async function(object) {
-                const urlDetails = `${endpoint}/${object.objectNumber}?key=${apiKey}`;
-
-                try {
-                    // Fetch details
-                    const response = await fetch(urlDetails);
-                    const jsonData = await response.json();
-                    // filter data
-                    const filteredData = await dataModule.filter(jsonData.artObject);
-                    return await filteredData[0];
-                } catch (err) {
-                    console.log('Error: ', err);
-                }
-            });
-
-            // wait until all the data from the details is fetched.
-            let allData = await Promise.all(details);
-            // store all the data in the localStorage.
-            await dataModule.setItem('details', allData);
-            // return fetched data.
-            return await allData;
-        } catch (err) {
-            console.log('Error: ', err);
-        }
-    }
-};
+module.exports = { overview, details };
--- docs/src/js/modules/render.js
+++ docs/src/js/modules/render.js
@@ -1,141 +1,26 @@
-const objects = document.querySelector('.objects'),
-    details = document.querySelector('section');
+function overview(res, title, data) {
+    return res.render('overview', {
+        styles: './css/index.css',
+        title: title,
+        data
+    });
+}
 
-export const render = {
-    /**
-     * Function to render the loading image.
-     *
-     * @param elementId
-     */
-    loader: function (elementId) {
-        const element = document.getElementById(elementId),
-            loadingImage = `
-                <div class="loading">
-                    <img src="./img/loading.gif" alt="loading">
-                    <p>Loading...</p>
-                </div>
-            `;
+function details(res, title, data) {
+    return res.render('details', {
+        styles: './../css/index.css',
+        title: title, // We use this for the page title, see views/partials/head.ejs
+        data
+    });
+}
 
-        element.insertAdjacentHTML("beforeend", loadingImage);
-    },
+function search(res, title, query, data) {
+    return res.render('results', {
+        styles: './css/index.css',
+        title: title,
+        query: query, // We use this for the page title, see views/partials/head.ejs
+        data
+    });
+}
 
-    /**
-     * Function to render the overview of all the paintings.
-     *
-     * @param data
-     */
-    overview: function (data) {
-        const html = `
-            <a href="#${data.id}" data-route="${data.id}" class="link">
-                <div class="object">
-                    <img src="${data.imgUrl}">
-                    <div class="title">
-                        <h3 class="title-name">${data.title}</h3>
-                    </div>
-                </div>
-            </a>
-        `;
-
-        objects.insertAdjacentHTML('beforeend', html);
-    },
-
-    /**
-     * Function to switch between html layouts depending if there are colors or not.
-     *
-     * @param data
-     */
-    details: function (data) {
-        // iterate over the colors to get all the colors and the name of an object
-        if (data.colors.length > 0) {
-            this.detailsWithColors(data);
-        } else {
-            this.detailsNoColors(data);
-        }
-    },
-
-    /**
-     * Function to render content with colors.
-     *
-     * @param data
-     */
-    detailsWithColors: function (data) {
-        const html = `
-            <div data-route="${data.id}">
-                <div class="object">
-                    <div class="title">
-                        <h3 class="title-name">${data.title}</h3>
-                        <span class="title-objectnumber">${data.id}</span>
-                    </div>
-                    <div class="presenting-date">
-                        <span>Year the painting was presented: <span class="date">${data.presentingDate}</span></span>
-                    </div>
-                    <div class="colors"> 
-                        <span>Colors:</span>
-                         <ul>
-                            <!--
-                                loop through the colors in the array and place them in a li tag
-                                used .join('') on the map to remove apostrophe
-                            -->
-                            ${data.colors.map(colors => {
-                                return `<li class="hex-color" style="background-color: ${colors.hex}">${colors.hex}</li>`
-                            }).join('')}
-                         </ul>
-                    </div>
-                </div>
-            </div>
-        `;
-
-        details.insertAdjacentHTML('beforeend', html);
-    },
-
-    /**
-     * Function to render content that has no colors defined.
-     *
-     * @param data
-     */
-    detailsNoColors: function (data) {
-        const html = `
-            <div data-route="${data.id}">
-                <div class="object">
-                    <div class="title">
-                        <h3 class="title-name">${data.title}</h3>
-                        <span class="title-objectnumber">${data.id}</span>
-                    </div>
-                    <div class="presenting-date">
-                        <span>Year the painting was presented: <span class="date">${data.presentingDate}</span></span>
-                    </div>
-                    <div class="colors">
-                        <span>Colors: </span><span class="not-defined">not defined</span>
-                    </div>
-                </div>
-            </div>
-        `;
-
-        details.insertAdjacentHTML('beforeend', html);
-    },
-
-    /**
-     * No data found html renderer.
-     */
-    noDataFound: function () {
-        const html = `
-            <div class="no-data"><span>Oeps! Het ziet er naar uit dat deze schilder niet bestaat. Check voor de zekerheid of je de naam goed gesachreven hebt.</span></div>
-        `;
-
-        objects.insertAdjacentHTML('beforeend', html);
-    },
-
-    /**
-     * Function to remove all the content inside a div.
-     *
-     * @param elementId: the ID of the div that you want to clear.
-     */
-    remove: function (elementId) {
-        const div = document.getElementById(elementId);
-
-        while(div.firstChild) {
-            div.removeChild(div.firstChild);
-        }
-    }
-
-};
+module.exports = { overview, details, search };
