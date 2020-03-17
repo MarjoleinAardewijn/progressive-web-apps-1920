d4ed03c8133228d3acaca2927850cb80b7fbfb5a
MarjoleinAardewijn <marjolein.aardewijn@gmail.com>
2020-03-17 14:00:09 +0100

    SW setup

--- scripts/build-static-assets.js
+++ scripts/build-static-assets.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 
 return gulp.src([
     './src/img/**/*.*',
-    // './src/service-worker.js',
+    './src/service-worker.js',
     './src/manifest.json',
 ])
     .pipe(gulp.dest('./static/'));
