64a3c62053cbebee2ca9b54133ac3e8cb72f4980
MarjoleinAardewijn <marjolein.aardewijn@gmail.com>
2020-03-24 19:06:23 +0100

    refactor functions to ES6 function for consistancy in code

--- scripts/modules/render.js
+++ scripts/modules/render.js
@@ -1,25 +1,25 @@
-function basicPage(res, view, title, revManifest) {
-    res.render(view, {
-        title,
-        revManifest
-    })
-}
+const basicPage = (res, view, title, revManifest) => {
+        res.render(view, {
+            title,
+            revManifest
+        })
+    },
 
-function pageWithData(res, view, title, data, revManifest) {
-    return res.render(view, {
-        title,
-        data,
-        revManifest
-    });
-}
+    pageWithData = (res, view, title, data, revManifest) => {
+        return res.render(view, {
+            title,
+            data,
+            revManifest
+        });
+    },
 
-function search(res, title, query, data, revManifest) {
-    return res.render('results', {
-        title,
-        query,
-        data,
-        revManifest
-    });
-}
+    search = (res, title, query, data, revManifest) => {
+        return res.render('results', {
+            title,
+            query,
+            data,
+            revManifest
+        });
+    };
 
 module.exports = { basicPage, pageWithData, search };
