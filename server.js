d3dfc98133465cfc34a52715fe38e3a2d2049851
MarjoleinAardewijn <marjolein.aardewijn@gmail.com>
2020-03-24 19:05:04 +0100

    browser caching

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const config = require('./config');
 
 // Require (third-party) modules
-const compression = require('compression'),
-    express = require('express'),
+const express = require('express'),
+    compression = require('compression'),
     revManifest = require('./static/rev-manifest'),
     render = require('./scripts/modules/render'),
     router = require('./scripts/modules/router'),
     // Create new express app in 'app'
-    app = express();
+    app = express(),
+    // function for setting cache-control headers
+    cacheMiddleWare = (req, res, next) => {
+        res.setHeader('Cache-Control', 'max-age=365000000, immutable');
+        next();
+    };
 
-// Link the templating engine to the express app
-app.set('view engine', 'ejs')
+    // gzip files
+app.use(compression())
+    // cache css file (browser)
+    .use(/.*-[0-9a-f]{10}\..*/, cacheMiddleWare)
+
+    // Link the templating engine to the express app
+    .set('view engine', 'ejs')
     // Tell the views engine/ejs where the template files are stored (SettingName, value)
     .set('views', 'views')
 
@@ -19,10 +29,7 @@ app.set('view engine', 'ejs')
     // Sending something (responding) ends the response cycle
     .use(express.static('static'))
 
-    // Gzip files
-    .use(compression())
-
-    .get('/', function(req, res) {
+    .get('/', (req, res) => {
         render.basicPage(res, 'home', 'Rijksmuseum Schilders', revManifest);
     })
 
@@ -30,18 +37,18 @@ app.set('view engine', 'ejs')
         render.basicPage(res, 'offline', 'Oeps! Je bent offline.', revManifest);
     })
 
-    .get('/rembrandt-van-rijn', function(req, res) {
+    .get('/rembrandt-van-rijn', (req, res) => {
         router.overview(res, revManifest);
     })
 
-    .get('/schilderij/:id', function(req, res) {
+    .get('/schilderij/:id', (req, res) => {
         router.details(res, req.params.id, revManifest);
     })
 
-    .get('/search', function(req, res) {
+    .get('/search', (req, res) => {
         router.search(res, req.query.artist, revManifest);
     })
     // Actually set up the server
-    .listen(config.port, function() {
+    .listen(config.port, () => {
         console.log(`Application started on port: ${config.port}`);
     });
